9fdfce0b037746798e01d96370a4548b4a67c11d
Mohammad Issawi <2000issawi@gmail.com>
2022-10-10 16:21:20 +0300

    Handle nullptr wrappee

--- src/MeshBase.cpp
+++ src/MeshBase.cpp
@@ -9,16 +9,25 @@ MeshBase::MeshBase(unique_ptr<MeshBase> wrappee) : wrappee(move(wrappee))
 }
 const vector<vec3> &MeshBase::getPositions()
 {
+    if (wrappee == nullptr)
+        return positions;
     return wrappee->getPositions();
 }
 const vector<vec3> &MeshBase::getNormals()
 {
+    if (wrappee == nullptr)
+        return normals;
     return wrappee->getNormals();
 }
 const vector<vec2> &MeshBase::getUvCoords0()
 {
+    if (wrappee == nullptr)
+        return uvCoords0;
     return wrappee->getUvCoords0();
 }
-const vector<unsigned> &MeshBase::getIndices(){
+const vector<unsigned> &MeshBase::getIndices()
+{
+    if (wrappee == nullptr)
+        return indices;
     return wrappee->getIndices();
 }
