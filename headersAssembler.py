d35aa6bad724ed54182d3b59c6a3c107b4a64d47
Mohammad Issawi <2000issawi@gmail.com>
2022-10-11 01:19:56 +0300

    Setup library installation

---
+++ headersAssembler.py
@@ -0,0 +1,12 @@
+#!/bin/python3
+
+import os
+from posix import listdir
+from posixpath import join
+cwd = os.getcwd()
+cwd = cwd+'/include'
+h_files = [f for f in listdir(cwd) if os.path.isfile(
+    join(cwd, f)) and os.path.splitext(f)[1] == '.h']
+for f in h_files:
+    print('./include/'+f)
+
